11d9a94576eb4dff1eff15def44d261d070dee91
nandobas <fernando.basilio@gmail.com>
2022-06-26 18:29:56 -0300

    to docs

---
+++ src/views/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <v-card>
+      <v-card-title class="headline grey lighten-2">
+        Rezar
+      </v-card-title>
+
+      <v-card-text>
+        Sistema desenvolvido por Fernando Basilio <br />
+        O intuito da ferramenta é facilitar a oração do terço católico.<br />
+        contato:
+        <a href="mailto:fernando.basilio@gmail.com"
+          >fernando.basilio@gmail.com</a
+        >
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
